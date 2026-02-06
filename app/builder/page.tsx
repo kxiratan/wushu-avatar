bf5127f1989504408e2e5664685e218da084a38b
keira <keira.tan@berkeley.edu>
2026-02-06 00:00:59 -0800

    added functionality to choosing silk and weapon, added competition time button with no functionality yet

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -10,6 +10,9 @@ export default function AvatarBuilder() {
   const [hoveredSilk, setHoveredSilk] = useState<string | null>(null);
   const [hoveredWeapon, setHoveredWeapon] = useState<string | null>(null);
 
+  const [selectedSilk, setSelectedSilk] = useState<string | null>(null);
+  const [selectedWeapon, setSelectedWeapon] = useState<string | null>(null);
+
   return (
     <main className="min-h-screen bg-white grid-background p-8 flex flex-col">
       
@@ -46,6 +49,7 @@ export default function AvatarBuilder() {
                 src="red-and-white-silk.png"
                 alt="red and white silk"
                 className="w-auto h-[300px] hover:rotate-4"
+                onClick={() => setSelectedSilk("red-and-white-silk.png")}
               />
 
               {hoveredSilk === "red" && (
@@ -67,6 +71,7 @@ export default function AvatarBuilder() {
                 src="straightsword.png"
                 alt="straight sword"
                 className="w-auto h-[100px] hover:rotate-4"
+                onClick={() => setSelectedWeapon("straightsword.png")}
               />
 
               {hoveredWeapon === "straight" && (
@@ -81,11 +86,31 @@ export default function AvatarBuilder() {
         </div>
 
         {/* Center — stick figure */}
-        <img
-            src="stick-figure.png"
-            alt="stick figure"
-            className="w-auto h-[300px] hover:rotate-4"
-        />
+        <div className="relative flex items-center justify-center">
+            <img
+              src="stick-figure.png"
+              alt="stick figure"
+              className="w-auto h-[300px] hover:rotate-4"
+            />
+
+            {selectedSilk && (
+              <img
+                src={selectedSilk}
+                alt="selected silk"
+                className="absolute top-13 left-1/2 -translate-x-1/2 h-[280px] w-auto pointer-events-none"
+              />
+            )}
+
+            {selectedWeapon && (
+              <img
+                src={selectedWeapon}
+                alt="selected weapon"
+                className="absolute top-30 right-27 h-[100px] w-auto"
+              />
+            )}
+
+        </div>
+
 
         {/* Right side */}
         <div className="flex items-center justify-start w-[200px]">
@@ -99,6 +124,7 @@ export default function AvatarBuilder() {
                 src="blue-and-white-silk.png"
                 alt="blue and white silk"
                 className="w-auto h-[300px] hover:rotate-4"
+                onClick={() => setSelectedSilk("blue-and-white-silk.png")}
               />
 
               {hoveredSilk === "blue" && (
@@ -120,6 +146,7 @@ export default function AvatarBuilder() {
                 src="broadsword.png"
                 alt="broad sword"
                 className="w-auto h-[80px] hover:rotate-4"
+                onClick={() => setSelectedWeapon("broadsword.png")}
               />
 
               {hoveredWeapon === "broad" && (
@@ -133,6 +160,18 @@ export default function AvatarBuilder() {
           )}
         </div>
       </div>
+
+      {selectedSilk && selectedWeapon && (
+        <div className="flex justify-center">
+          <button
+            onClick={() => {
+
+            }}
+            className="px-14 py-2 text-xl hover:rotate-3 transition-transform cursor-pointer bg-[#e63946] rounded-lg text-[#fdc921]">
+            competition time!
+          </button>
+        </div>
+      )}
 
     </main>
   );
